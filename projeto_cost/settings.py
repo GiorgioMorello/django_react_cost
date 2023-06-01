effeb452efd679ddb77557a20132348d413d0fac
Giorgio <morellogiorgio327@gmail.com>
2023-05-31 23:22:14 -0300

    changin allowed hosts

--- projeto_cost/settings.py
+++ projeto_cost/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.0.111', '127.0.0.1', '0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
