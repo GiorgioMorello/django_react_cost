de2ce657ad0423478629e06f2001d3496273872c
Giorgio <morellogiorgio327@gmail.com>
2023-05-31 23:05:36 -0300

    .

--- projeto_cost/settings.py
+++ projeto_cost/settings.py
@@ -25,7 +25,7 @@ load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if os.getenv('DEBUG') == "1" else False
+DEBUG = False
 
 ALLOWED_HOSTS = ['192.168.0.111', '127.0.0.1', '0.0.0.0', 'localhost']
 
