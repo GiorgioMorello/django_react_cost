27418d8e43b7df4ff70d6ae871aaaad5b2d6b815
Giorgio <morellogiorgio327@gmail.com>
2023-06-01 22:16:49 -0300

    access admin

--- projeto_cost/settings.py
+++ projeto_cost/settings.py
@@ -142,3 +142,13 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+CORS_ALLOWED_ORIGINS = [
+    'https://djangoreactcost-production.up.railway.app/',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://djangoreactcost-production.up.railway.app/'
+]
